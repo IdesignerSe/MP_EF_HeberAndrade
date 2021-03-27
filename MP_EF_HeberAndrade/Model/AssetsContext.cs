f4c523957f2dfab33122012a19b3df5d8b07d4a6
idesignerse <heberandrade@gmail.com>
2021-03-27 20:39:58 +0100

    Update
    Update

--- MP_EF_HeberAndrade/Model/AssetsContext.cs
+++ MP_EF_HeberAndrade/Model/AssetsContext.cs
@@ -54,9 +54,86 @@ namespace MP_EF_HeberAndrade
                 }
 
                 return list;
+            }
+        }
+        public Computer GetPostById(int postId)
+        {
+            var sql = @"SELECT [Id], [Brand], [ModelName], [PurchaseDate], [InicialCost], [ExpiredDate], [ExpiredCost]
+                        FROM Computer";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", postId));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var bp = new Computer
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Brand = reader.GetSqlString(1).Value,
+                        ModelName = reader.GetSqlString(2).Value,
+                        PurchaseDate = reader.GetSqlInt32(3).Value,
+                        InicialCost = reader.GetSqlInt32(4).Value,
+                        ExpiredDate = reader.GetSqlInt32(5).Value,
+                        ExpiredCost = reader.GetSqlInt32(6).Value,
+                    };
+                    return bp;
+
+                }
+
+                return null;
 
             }
         }
+
+        public void CreateBlogpost(Computer computer)
+        {
+            //INSERT INTO Computer(Brand,ModelName) VALUES('Good post','Mats Lind')
+            //INSERT INTO BlogPost(Title,Author) VALUES('Good post','Mats Lind')
+            var sql = "INSERT INTO Computer(Brand,ModelName) VALUES(@Brand,@ModelName)";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Brand", computer.Brand));
+                command.Parameters.Add(new SqlParameter("Title", computer.ModelName));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateBlogpost(Computer computer)
+        {
+            var sql = "UPDATE BlogPost SET Title=@Title WHERE id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", blogPost.Id));
+                command.Parameters.Add(new SqlParameter("Title", blogPost.Title));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteBlogpost(Computer computer)
+        {
+            //DELETE FROM BlogPost WHERE Id=3
+            var sql = "DELETE FROM BlogPost WHERE id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", computer.Id));
+                command.ExecuteNonQuery();
+            }
+        }
+
     }
    
 }
