9b5268eb85dfe1dc495d720600eb2d938c0e3a77
idesignerse <heberandrade@gmail.com>
2021-03-29 00:32:14 +0200

    Update Program.cs

--- MP_EF_HeberAndrade/Program.cs
+++ MP_EF_HeberAndrade/Program.cs
@@ -8,61 +8,185 @@ namespace MP_EF_HeberAndrade
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var dbContext = new AssetsContext();
-
+        AssetsContext context;
+        IEnumerable<Asset> list;
+         void Run()
             {
-                Computer computerItem = new Computer("MacBook", "2018 15 inch ", 20180101, 13000, 20211201, 8000);
-                string classBrand = computerItem.GetType().Name;
-                PropertyInfo brand = computerItem.GetType().GetProperty("Brand");
-                List<PropertyInfo> properties = computerItem.GetType().GetProperties().ToList();
+                new App();
+                PageMainMenu();
+                CreatePost();
+                PageUpdatePost();
+                DeletePost();
+                ShowAllBlogPostsBrief();
+            }
 
-                Computer computerItem2 = new Computer("MacBook", "2019 15 inch ", 20180101, 16000, 20211201, 10000);
-                List<Computer> computers = new List<Computer>();
+             void PageMainMenu()
+            {
+                Header("Menu");
 
-                computers.Add(computerItem);
-                computers.Add(computerItem2);
-                computers = dbContext.Computers.ToList();
+                ShowAllBlogPostsBrief();
 
-                Dictionary<string, int> colWidths = new Dictionary<string, int>();
+                WriteLine("Press key A,B,C or D Instructions: ");
+                WriteLine("a) Back Menu");
+                WriteLine("b) Create an Item");
+                WriteLine("c) Update and Item");
+                WriteLine("d) Delete and Item");
 
-                foreach (PropertyInfo property in properties)
+                ConsoleKey command = Console.ReadKey(true).Key;
+
+                if (command == ConsoleKey.A)
+                    PageMainMenu();
+
+                if (command == ConsoleKey.B)
+                    CreatePost();
+
+                if (command == ConsoleKey.C)
+                    PageUpdatePost();
+
+                if (command == ConsoleKey.D)
+                    DeletePost();
+            }
+             void CreatePost()
+            {
+                Header("Create");
+
+                ShowAllBlogPostsBrief();
+
+                Write("Write an Item: ");
+
+                string newBrand = Console.ReadLine();
+                //
+
+                Write("Write model and year: ex. Macbest 2030 ");
+
+                string newModelName = Console.ReadLine();
+                //
+
+                Write("Write the date of purchase! ex. 2021/12/01");
+
+                string newPurchaseDate = Console.ReadLine();
+
+                //
+                Write("Write the inicial cost! ex. 13,000");
+
+                string newInitialCost = Console.ReadLine();
+
+                //
+                Write("Write the Expiration date! # years from now ex.2023/12/01");
+
+                string newExpiredDate = Console.ReadLine();
+
+                //
+
+                Write("Write the Expiration Price! To sale out");
+
+                string newExpiredCost = Console.ReadLine();
+
+
+                var asset = new Asset();
+
+                asset.Brand = newBrand;
+                asset.ModelName = newModelName;
+                asset.PurchaseDate = newPurchaseDate;
+                asset.InicialCost = newInitialCost;
+                asset.ExpiredDate = newExpiredDate;
+                asset.ExpiredCost = newExpiredCost;
+
+                using (context = new AssetsContext())
                 {
-                    int colLength = property.Name.Length;
-                    int tmpLength = 0;
-                    int dataLength = 0;
+                    context.Computers.Add((Computer)asset);
+                    context.SaveChanges();
+                }
+                Write("Item is now in our list! ");
+                Console.ReadKey();
+                PageMainMenu();
+            }
+             void PageUpdatePost()
+            {
+                Header("Uppdatera");
 
-                    foreach (Computer computer in computers)
-                    {
-                        tmpLength = computers.Max(computer => computer.GetType().GetProperty(property.Name)).GetValue(computer).ToString().Length;
-                        dataLength = Math.Max(dataLength, tmpLength);
-                    }
-                    int maxLength = Math.Max(colLength, dataLength);
+                ShowAllBlogPostsBrief();
 
-                    colWidths.Add(property.Name, maxLength);
+                Write("Any Item to update? ");
+
+                int assetId = int.Parse(Console.ReadLine());
+
+                using (context = new AssetsContext())
+                {
+                    var asset = context.Computers.Find(assetId);
+
+                    WriteLine("The actual Item is: " + assetId);
+
+                    Write("Write a new Item: ");
+
+                    string newBrand = Console.ReadLine();
+
+                    asset.Brand = newBrand;
+
+                    context.SaveChanges();
                 }
 
+                Write("Bloggposten uppdaterad.");
+                Console.ReadKey();
+                PageMainMenu();
+            }
+             void DeletePost()
+            {
+                Header("DELETE");
+
+                ShowAllBlogPostsBrief();
+
+                Write("Wich Item to DELETE? ");
+
+                int assetId = int.Parse(Console.ReadLine());
+                using (context = new AssetsContext())
+                {
+                    var asset = context.Computers.Find(assetId);
+
+                    context.Remove(asset);
+                    context.SaveChanges();
+                }
+
+                Write("Asset Item is  DELETED.");
+                Console.ReadKey();
+                PageMainMenu();
+            }
+             void ShowAllBlogPostsBrief()
+            {
+                using (context = new AssetsContext())
+                {
+                    var list = context.Computers.ToList();
+                }
+                foreach (Asset bp in list)
+                {
+                    WriteLine(bp.Id.ToString().PadRight(5) + bp.Brand.PadRight(30) +
+                        bp.ModelName.PadRight(20) + bp.PurchaseDate.ToString().PadRight(5) +
+                        bp.InicialCost.ToString().PadRight(5) + bp.ExpiredDate.ToString().PadRight(5) +
+                        bp.ExpiredCost.ToString().PadRight(5)
+                    );
+                }
+                WriteLine();
+            }
+             void Header(string text)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine();
-
-                //Print Header with correct widths
-                foreach (var colWidth in colWidths)
-                {
-                    Console.Write(colWidth.Key.PadRight(colWidth.Value + 2));
-                }
-
+                Console.WriteLine(text.ToUpper());
                 Console.WriteLine();
-
-                //Print Data with correct widths
-                foreach (Computer computer in computers)
-                {
-                    foreach (var colWidth in colWidths)
-                    {
-                        var c = computer.GetType().GetProperties().Where(c => c.Name == colWidth.Key).FirstOrDefault();
-                        Console.Write(c.GetValue(computer).ToString().PadRight(colWidth.Value + 2));
-                    }
-                    Console.WriteLine();
-                }
+            }
+            void WriteLine(string text = "")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(text);
+            }
+            void Write(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(text);
             }
         }
     } 
